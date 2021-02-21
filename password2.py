6697e2aaf0f1dc6f3473a88aeeb15ab3c277d831
Josh Greene <56303932+JoshGreene1@users.noreply.github.com>
2021-02-22 12:08:46 +1300

    Create password2.py

---
+++ password2.py
@@ -0,0 +1,15 @@
+website = []
+password = []
+
+global user
+
+print("Welcome to your Password Manager")
+print("Are you a new or exisiting user?")
+user = input("New or Exisiting")
+
+def new():
+    if user == "New":
+        print("Condtions for using this software are as follows\nYou must be over the age of 13\n")
+
+
+new()
