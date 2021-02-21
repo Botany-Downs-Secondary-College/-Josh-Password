9dc82e4368d6e91aa05fde6d94528dbe428fd4fd
Josh Greene <56303932+JoshGreene1@users.noreply.github.com>
2021-02-22 12:20:36 +1300

    Create password3.py

---
+++ password3.py
@@ -0,0 +1,27 @@
+website = []
+password = []
+
+y = 1
+global user
+
+
+def new():
+    if user == "New":
+        print("Condtions for using this software are as follows\nYou must be over the age of 13\n")
+        age = int(input("Please Insert your age (Decimals will not be accepted): "))
+        if age < 13:
+            print("Yesyesyesy")
+
+
+new()
+
+print("Welcome to your Password Manager")
+print("Are you a new or exisiting user?")
+user = input("New or Exisiting: ")
+#while y >= 1:
+ #   user = input("New or Exisiting: ")
+  #  if user == "New":
+   #     y + 1
+    #    new()
+    #else:
+     #   print("Please enter a correct response")
